5e9b1bc87672e80b224d2f557fd8ab4908b89b28
Mitchell Robles <mitchellr92@live.com>
2019-12-21 10:14:22 -0800

    continuing to try and get the x functionality working

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const innerBoxes = document.querySelectorAll(".inner");
+let innerBoxes = document.querySelectorAll(".inner");
 
 // Just making boxes clickable
 
+// for (let i = 0; i < innerBoxes.length; i++) {
+//   console.log(innerBoxes[i]);
+// }
+
 function clickBox() {
-  console.log("box has been clicked!");
   this.classList.add("has-ex");
+
+  innerBoxes = Array.from(innerBoxes);
+  console.log(innerBoxes);
+
+  // let randBox = innerBoxes[Math.floor(Math.random() * innerBoxes.length)];
+  // randBox.classList.add("has-circle");
 }
 
 innerBoxes.forEach(box => box.addEventListener("click", clickBox));
