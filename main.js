a5f0feb70d47fc4947042737bfa6fadd3e6f5194
Mitchell Robles <mitchellr92@live.com>
2019-12-21 10:40:08 -0800

    game working with circles being added randomly by computer

--- main.js
+++ main.js
@@ -10,10 +10,19 @@ function clickBox() {
   this.classList.add("has-ex");
 
   innerBoxes = Array.from(innerBoxes);
-  console.log(innerBoxes);
+  let clickedBox;
 
-  // let randBox = innerBoxes[Math.floor(Math.random() * innerBoxes.length)];
-  // randBox.classList.add("has-circle");
+  console.log(innerBoxes);
+  for (let i = 0; i < innerBoxes.length; i++) {
+    if (innerBoxes[i].classList.length === 3) {
+      clickedBox = innerBoxes[i];
+      let index = innerBoxes.indexOf(clickedBox);
+      innerBoxes.splice(index, 1)
+    }
+  }
+
+  let randBox = innerBoxes[Math.floor(Math.random() * innerBoxes.length)];
+  randBox.classList.add("has-circle");
 }
 
 innerBoxes.forEach(box => box.addEventListener("click", clickBox));
